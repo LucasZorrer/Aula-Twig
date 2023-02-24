24fc67613724af517a02c0ff021fbefcde165815
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-02-24 09:41:41 -0300

    Initial commit

---
+++ produtos.php
@@ -0,0 +1,13 @@
+<?php
+
+require('vendor/autoload.php');
+
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+$twig = new \Twig\Environment($loader);
+
+$template = $twig->load('produtos.html');
+
+echo $template->render([
+    'title' => "Produtos",
+]);
