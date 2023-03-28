91163929f9b2078fb99656097b1e05a708148be5
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-03-28 14:12:28 -0300

    sanitiza

--- documentos_novo.php
+++ documentos_novo.php
@@ -1,9 +1,13 @@
 <?php
 
 require("twig-carregar.php");
+require('func/sanitize_filename.php');
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST' && !$_FILES['arquivo']['error']){
-    move_uploaded_file($_FILES['arquivo']['tmp_name'], 'uploads/' .  $_FILES['arquivo']['name']);
+
+    $arquivo = sanitize_filename($_FILES['arquivo']['name']);
+
+    move_uploaded_file($_FILES['arquivo']['tmp_name'], 'uploads/' . $arquivo);
 }
 
 echo $twig->render('documentos_novo.html');
